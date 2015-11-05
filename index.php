c3ee32e25b21e40f69eaf10de30831ec285394bb
Myroslav Stelmashchuk <m.stelmashchuk@grt-team.com>
2015-11-05 10:16:36 +0200

    Builder Pattern Development

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     require_once 'patterns/facade.php';
     require_once 'patterns/mediator.php';
     require_once 'patterns/singleton.php';
+    require_once 'patterns/builder.php';
 ?>
 
 <html>
@@ -74,6 +75,32 @@
 
                     $Lee->borrowBook();
                     echo "<p>Lee borrows a book again: {$Lee->getAuthorAndTitle()}</p>";
+                ?>
+            </div>
+            <div class="builder-pattern lesson-block">
+                <h3>Page Builder <span>(creational)</span><em>/patterns/builder.php</em></h3>
+                <p class="description">
+                    Separate the construction of a complex object from its representation so that
+                    the same construction process can create different representations.
+                </p>
+                <p class="small-description">
+                    Imagine you want to create some page builder. <strong>Builder</strong> classes is the structure of pages.
+                    <strong>Page director</strong> is the content of pages. In this case there is one page director to
+                    both pages. You can use different directors. Depending on what you want you can construct the needed page.
+                    Also you can define which methods should be used in Builder and in Director classes by defining them in Abstract classes.
+                </p>
+                <?php
+                    $pageBuilder = new OneColumnPageBuilder();
+                    $pageDirector = new PageDirector($pageBuilder);
+                    $pageDirector->buildPage();
+                    $page = $pageDirector->getPage();
+                    echo $page->showPage();
+                    echo "<hr />";
+                    $pageWithSidebarBuilder = new TwoColumnsRightPageBuilder();
+                    $pageDirector = new PageDirector($pageWithSidebarBuilder);
+                    $pageDirector->buildPage();
+                    $pageTwoColumnsFight = $pageDirector->getPage();
+                    echo $pageTwoColumnsFight->showPage();
                 ?>
             </div>
         </main>
